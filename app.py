0837bc1c3ff1ad3049d3b87493e5527dc0b80673
student-Phulmati <phulmatis60@gmail.com>
2026-05-21 08:24:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,11 @@ from reportlab.graphics.shapes import Drawing, Line, String
 
 import plotly.graph_objects as go
 import plotly.express as px
-
+st.set_page_config(
+    page_title="App",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 # =====================================================
 # APP CONFIG
 # =====================================================
