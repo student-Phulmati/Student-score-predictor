c88bac004dd1d711e1845a6b95115baefe642c35
student-Phulmati <phulmatis60@gmail.com>
2026-05-25 00:27:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -619,15 +619,7 @@ def apply_css():
         text-shadow: {uploader_shadow} !important;
     }}
 
-    /* Browse/Upload button inside dropzone */
-    # [data-testid="stFileUploaderDropzone"] button,
-    # [data-testid="baseButton-secondary"] {{
-    #     color: #03045e !important;
-    #     background: rgba(255,255,255,0.95) !important;
-    #     border: 1px solid {uploader_border} !important;
-    #     font-weight: 900 !important;
-    #     border-radius: 8px !important;
-    # }}
+  
 
     /* ══ Profile page col-2 top fix ══ */
     .profile-info-card {{
